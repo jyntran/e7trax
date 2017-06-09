02116e9675bbc238e9f33a0bc340bf8078aaf205
J. Tran <j.yn.tran@gmail.com>
2017-06-09 04:03:22 +0000

    Fix issues with loading the build folder

--- e7trax/src/server/app.js
+++ e7trax/src/server/app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(express.static('./public'));
 app.use(express.static(path.join(__dirname, '../../build')));
-app.use('/spotify', routes);
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
+app.use('/spotify', routes);
+app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
